35589d8a72e1b6db1e505e185632896dfa2382e3
benhensor <benhensor@gmail.com>
2024-02-20 12:04:03 +0000

    deploying to vercel

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ['https://seasonit-v2-server.vercel.app'],
+    credentials: true,
+}))
 
 
 // Use routes
